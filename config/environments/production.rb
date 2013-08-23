21a4ccc17823e3adf5a5c28787119fedc03f8185
John Hamelink <john@johnhamelink.com>
2013-08-23 23:37:53 +0100

    The final straw @_@

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,10 @@ ScottishEntrepreneurialSupportNetwork::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+  config.assets.compress =false
+
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :closure
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
