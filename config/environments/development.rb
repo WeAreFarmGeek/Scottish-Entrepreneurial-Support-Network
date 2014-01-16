85ad5e58050562396bf743fd8aed85f1777e1b93
John Hamelink <hello@farmer.io>
2014-01-16 11:06:03 +0000

    Enable logstash

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ ScottishEntrepreneurialSupportNetwork::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.logstasher.enabled = true
+  config.logstasher.suppress_app_log = false
 end
