d938b01597dd42363884d96d9f8dcd67551ed779
John Hamelink <hello@farmer.io>
2014-03-31 15:30:17 +0100

    Added filters to embed view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,22 @@
 class HomeController < ApplicationController
+   before_filter :get_data, only: [:index, :embed]
    after_action :allow_iframe, :only => :embed
 
    def index
-    @categories = Category.all.inject([]) {|r,i| r.push(i.name) }.unshift(nil)
-    @tags       = Tag.all.inject([]) {|r,i| r.push(i.name) }.unshift(nil)
-    @search_types = SearchType.all
-  end
+   end
 
   def embed
-    @embed      = true
-    @categories = Category.all.inject([]) {|r,i| r.push(i.name) }.unshift(nil)
-    @tags       = Tag.all.inject([]) {|r,i| r.push(i.name) }.unshift(nil)
-    @search_types = SearchType.all
+    @embed = true
     render :index, :layout => 'embed'
   end
 
   private
+
+  def get_data
+    @categories   = Category.all.inject([]) {|r,i| r.push(i.name) }.unshift(nil)
+    @tags         = Tag.all.inject([]) {|r,i| r.push(i.name) }.unshift(nil)
+    @search_types = SearchType.all
+  end
 
   def allow_iframe
     response.headers.delete('X-Frame-Options')
